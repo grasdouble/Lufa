b394b34ed555606bc85ef321b3940d8a1d4a70c1
noofreuuuh <dev.n2ofr@gmail.com>
2025-03-15 22:13:03 +0100

    fix(workflow): change report destination and file extension

--- .github/scripts/generateOutdatedReport.js
+++ .github/scripts/generateOutdatedReport.js
@@ -2,7 +2,7 @@ const { exec } = require("child_process");
 const fs = require("fs");
 
 exec("pnpm outdated -r", (err, stdout, stderr) => {
-  fs.writeFile("REPORT_OUTDATED_DEPENDENCIES.md", stdout, (err) => {
+  fs.writeFile("reports/OUTDATED_DEPENDENCIES.report", stdout, (err) => {
     if (err) {
       console.error(`File write error : ${err}`);
       return;
