289722fad15ba133827202f03da09342ee0ad82c
noofreuuuh <dev.n2ofr@gmail.com>
2025-03-15 22:34:58 +0100

    fix(workflow): adapt script for outdated deps

--- .github/scripts/generateOutdatedReport.js
+++ .github/scripts/generateOutdatedReport.js
@@ -2,10 +2,15 @@ const { exec } = require("child_process");
 const fs = require("fs");
 
 exec("pnpm outdated -r", (err, stdout, stderr) => {
-  fs.writeFile("reports/OUTDATED_DEPENDENCIES.report", stdout, (err) => {
+  const reportContent = `# Outdated Dependencies Report\n\n${
+    stdout.trim() || "No outdated dependency"
+  }`;
+
+  fs.writeFile("reports/OUTDATED_DEPENDENCIES.report", reportContent, (err) => {
     if (err) {
       console.error(`File write error : ${err}`);
       return;
     }
+    console.log("Report generated successfully.");
   });
 });
