4d6bafc45f973a052731d846e3647fe0817cad6f
Noofreuuuh <dev.n2ofr@gmail.com>
2025-03-15 21:50:59 +0100

    chore: add deps auto update workflow (#2)

--- .github/scripts/generateOutdatedReport.js
+++ .github/scripts/generateOutdatedReport.js
@@ -1,27 +1,11 @@
 const { exec } = require("child_process");
-const fs = require("fs").promises;
+const fs = require("fs");
 
-const generateReport = async () => {
-  try {
-    const { stdout, stderr } = await new Promise((resolve, reject) => {
-      exec("pnpm outdated -r", (err, stdout, stderr) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve({ stdout, stderr });
-        }
-      });
-    });
-
-    const report = `# Outdated Dependencies Report\n\n${
-      stdout || "No outdated dependencies identified"
-    }`;
-
-    await fs.writeFile("REPORT_OUTDATED_DEPENDENCIES.md", report);
-    console.log("Report generated successfully.");
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-  }
-};
-
-generateReport();
+exec("pnpm outdated -r", (err, stdout, stderr) => {
+  fs.writeFile("REPORT_OUTDATED_DEPENDENCIES.md", stdout, (err) => {
+    if (err) {
+      console.error(`File write error : ${err}`);
+      return;
+    }
+  });
+});
