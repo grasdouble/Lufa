575cce31cd03eaf34a551289fcf8cf8b07373090
noofreuuuh <noofreuuuh@users.noreply.github.com>
2025-03-25 20:17:36 +0000

    deploy: 2a8c5edfd4faf13da6174d1c42c4daa9092f9a23

---
+++ pr-31/assets/Avatar.stories-FNHbb3AR.js
@@ -0,0 +1,258 @@
+import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{A as t}from"./lufa-ui-D98RJmy4.js";const A={title:"2. OLD/Components/Avatar",component:t,parameters:{layout:"centered"},tags:[""]},m={avatar:{table:{category:"Avatar",disable:!0}},notification:{table:{category:"Notification",disable:!0}},size:{table:{category:"Notification",disable:!0}}},i={argTypes:{...m,size:{control:"select",description:"The size of the avatar",options:["xsmall","small","medium","large","xlarge"]},variant:{control:"radio",description:"The variant of the avatar",options:["circle","square"],name:"avatar.variant",table:{category:"Avatar",type:{summary:"circle | square"},defaultValue:{summary:"circle"}}},imgURL:{control:"text",description:"The image URL of the avatar",name:"avatar.imgURL",table:{category:"Avatar"},type:{required:!0,name:"string"}},color:{control:"radio",description:"The color of the notification",options:["none","green","orange","red","gray"],name:"notification.color",table:{category:"Notification",type:{summary:"none | green | orange | red | gray"},defaultValue:{summary:"none"}}},position:{control:"radio",description:"The position of the notification",options:["top","bottom"],name:"notification.position",table:{category:"Notification",type:{summary:"top | bottom"},defaultValue:{summary:"top"}}}},args:{size:"xlarge",variant:"circle",imgURL:"https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",color:"none",position:"top"},render:({variant:e,imgURL:r,color:b,position:j,...l})=>(l.avatar={variant:e,imgURL:r},l.notification={color:b,position:j},a.jsx(t,{...l}))},n={variant:"circle",imgURL:"https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"},c={variant:"square",imgURL:"https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"},o={argTypes:{...m,variant:{control:"radio",description:"The type of the avatar",options:["circle","square"],name:"avatar.variant",table:{type:{summary:"circle | square"},defaultValue:{summary:"circle"}}}},args:{variant:"circle"},render:({variant:e})=>a.jsxs("center",{children:[a.jsx("span",{style:{margin:"0.5em"},children:a.jsx(t,{avatar:{...n,variant:e},size:"xsmall"})}),a.jsx("span",{style:{margin:"0.5em"},children:a.jsx(t,{avatar:{...n,variant:e},size:"small"})}),a.jsx("span",{style:{margin:"0.5em"},children:a.jsx(t,{avatar:{...n,variant:e},size:"medium"})}),a.jsx("span",{style:{margin:"0.5em"},children:a.jsx(t,{avatar:{...n,variant:e},size:"large"})}),a.jsx("span",{style:{margin:"0.5em"},children:a.jsx(t,{avatar:{...n,variant:e},size:"xlarge"})})]})},s={argTypes:{...m,position:{control:"radio",description:"The position of the notification",options:["top","bottom"],name:"notification.position",table:{type:{summary:"top | bottom"},defaultValue:{summary:"top"}}}},args:{position:"top"},render:({position:e})=>{const r={position:e,color:"none"};return a.jsxs("center",{children:[a.jsxs("div",{className:"mb-5",children:[a.jsx("span",{style:{margin:"0.5em"},children:a.jsx(t,{avatar:n,notification:{...r,color:"green"},size:"xlarge"})}),a.jsx("span",{style:{margin:"0.5em"},children:a.jsx(t,{avatar:n,notification:{...r,color:"orange"},size:"xlarge"})}),a.jsx("span",{style:{margin:"0.5em"},children:a.jsx(t,{avatar:n,notification:{...r,color:"red"},size:"xlarge"})}),a.jsx("span",{style:{margin:"0.5em"},children:a.jsx(t,{avatar:n,notification:{...r,color:"gray"},size:"xlarge"})})]}),a.jsxs("div",{children:[a.jsx("span",{style:{margin:"0.5em"},children:a.jsx(t,{avatar:c,notification:{...r,color:"green"},size:"xlarge"})}),a.jsx("span",{style:{margin:"0.5em"},children:a.jsx(t,{avatar:c,notification:{...r,color:"orange"},size:"xlarge"})}),a.jsx("span",{style:{margin:"0.5em"},children:a.jsx(t,{avatar:c,notification:{...r,color:"red"},size:"xlarge"})}),a.jsx("span",{style:{margin:"0.5em"},children:a.jsx(t,{avatar:c,notification:{...r,color:"gray"},size:"xlarge"})})]})]})}};var p,g,d;i.parameters={...i.parameters,docs:{...(p=i.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    ...hiddenArgTypes,
+    size: {
+      control: 'select',
+      description: 'The size of the avatar',
+      options: ['xsmall', 'small', 'medium', 'large', 'xlarge']
+    },
+    variant: {
+      control: 'radio',
+      description: 'The variant of the avatar',
+      options: ['circle', 'square'],
+      name: 'avatar.variant',
+      table: {
+        category: 'Avatar',
+        type: {
+          summary: 'circle | square'
+        },
+        defaultValue: {
+          summary: 'circle'
+        }
+      }
+    },
+    imgURL: {
+      control: 'text',
+      description: 'The image URL of the avatar',
+      name: 'avatar.imgURL',
+      table: {
+        category: 'Avatar'
+      },
+      type: {
+        required: true,
+        name: 'string'
+      }
+    },
+    color: {
+      control: 'radio',
+      description: 'The color of the notification',
+      options: ['none', 'green', 'orange', 'red', 'gray'],
+      name: 'notification.color',
+      table: {
+        category: 'Notification',
+        type: {
+          summary: 'none | green | orange | red | gray'
+        },
+        defaultValue: {
+          summary: 'none'
+        }
+      }
+    },
+    position: {
+      control: 'radio',
+      description: 'The position of the notification',
+      options: ['top', 'bottom'],
+      name: 'notification.position',
+      table: {
+        category: 'Notification',
+        type: {
+          summary: 'top | bottom'
+        },
+        defaultValue: {
+          summary: 'top'
+        }
+      }
+    }
+  },
+  args: {
+    size: 'xlarge',
+    variant: 'circle',
+    imgURL: 'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    color: 'none',
+    position: 'top'
+  },
+  render: ({
+    variant,
+    imgURL,
+    color,
+    position,
+    ...args
+  }) => {
+    args.avatar = {
+      variant,
+      imgURL
+    };
+    args.notification = {
+      color,
+      position
+    };
+    return <Avatar {...args} />;
+  }
+}`,...(d=(g=i.parameters)==null?void 0:g.docs)==null?void 0:d.source}}};var v,f,y;o.parameters={...o.parameters,docs:{...(v=o.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  argTypes: {
+    ...hiddenArgTypes,
+    variant: {
+      control: 'radio',
+      description: 'The type of the avatar',
+      options: ['circle', 'square'],
+      name: 'avatar.variant',
+      table: {
+        type: {
+          summary: 'circle | square'
+        },
+        defaultValue: {
+          summary: 'circle'
+        }
+      }
+    }
+  },
+  args: {
+    variant: 'circle'
+  },
+  render: ({
+    variant
+  }: AvatarPropsAndCustomArgs) => {
+    return <center>
+                <span style={{
+        margin: '0.5em'
+      }}>
+                    <Avatar avatar={{
+          ...avatarCircle,
+          variant
+        }} size="xsmall" />
+                </span>
+                <span style={{
+        margin: '0.5em'
+      }}>
+                    <Avatar avatar={{
+          ...avatarCircle,
+          variant
+        }} size="small" />
+                </span>
+                <span style={{
+        margin: '0.5em'
+      }}>
+                    <Avatar avatar={{
+          ...avatarCircle,
+          variant
+        }} size="medium" />
+                </span>
+                <span style={{
+        margin: '0.5em'
+      }}>
+                    <Avatar avatar={{
+          ...avatarCircle,
+          variant
+        }} size="large" />
+                </span>
+                <span style={{
+        margin: '0.5em'
+      }}>
+                    <Avatar avatar={{
+          ...avatarCircle,
+          variant
+        }} size="xlarge" />
+                </span>
+            </center>;
+  }
+}`,...(y=(f=o.parameters)==null?void 0:f.docs)==null?void 0:y.source}}};var u,x,h;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  argTypes: {
+    ...hiddenArgTypes,
+    position: {
+      control: 'radio',
+      description: 'The position of the notification',
+      options: ['top', 'bottom'],
+      name: 'notification.position',
+      table: {
+        type: {
+          summary: 'top | bottom'
+        },
+        defaultValue: {
+          summary: 'top'
+        }
+      }
+    }
+  },
+  args: {
+    position: 'top'
+  },
+  render: ({
+    position
+  }: AvatarPropsAndCustomArgs) => {
+    const notification = {
+      position: position,
+      color: 'none'
+    };
+    return <center>
+                <div className="mb-5">
+                    <span style={{
+          margin: '0.5em'
+        }}>
+                        <Avatar avatar={avatarCircle} notification={{
+            ...notification,
+            color: 'green'
+          }} size="xlarge" />
+                    </span>
+                    <span style={{
+          margin: '0.5em'
+        }}>
+                        <Avatar avatar={avatarCircle} notification={{
+            ...notification,
+            color: 'orange'
+          }} size="xlarge" />
+                    </span>
+                    <span style={{
+          margin: '0.5em'
+        }}>
+                        <Avatar avatar={avatarCircle} notification={{
+            ...notification,
+            color: 'red'
+          }} size="xlarge" />
+                    </span>
+                    <span style={{
+          margin: '0.5em'
+        }}>
+                        <Avatar avatar={avatarCircle} notification={{
+            ...notification,
+            color: 'gray'
+          }} size="xlarge" />
+                    </span>
+                </div>
+                <div>
+                    <span style={{
+          margin: '0.5em'
+        }}>
+                        <Avatar avatar={avatarSquare} notification={{
+            ...notification,
+            color: 'green'
+          }} size="xlarge" />
+                    </span>
+                    <span style={{
+          margin: '0.5em'
+        }}>
+                        <Avatar avatar={avatarSquare} notification={{
+            ...notification,
+            color: 'orange'
+          }} size="xlarge" />
+                    </span>
+                    <span style={{
+          margin: '0.5em'
+        }}>
+                        <Avatar avatar={avatarSquare} notification={{
+            ...notification,
+            color: 'red'
+          }} size="xlarge" />
+                    </span>
+                    <span style={{
+          margin: '0.5em'
+        }}>
+                        <Avatar avatar={avatarSquare} notification={{
+            ...notification,
+            color: 'gray'
+          }} size="xlarge" />
+                    </span>
+                </div>
+            </center>;
+  }
+}`,...(h=(x=s.parameters)==null?void 0:x.docs)==null?void 0:h.source}}};const z=["Primary","AllSize","AllNotificationColors"],C=Object.freeze(Object.defineProperty({__proto__:null,AllNotificationColors:s,AllSize:o,Primary:i,__namedExportsOrder:z,default:A},Symbol.toStringTag,{value:"Module"}));export{C as A,o as a,s as b};
---
+++ pr-31/assets/Button.doc-Bg1l5W4V.js
@@ -0,0 +1,13 @@
+import{j as n}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as i}from"./index-CePRvBtH.js";import{M as e,P as a,C as c,S as r}from"./index-DpIZGlpI.js";import{B as h,A as m,a as p}from"./Button.stories-vxN664xF.js";import"./index-DmM0KDA7.js";import"./iframe-Dd0rPjSP.js";import"./index-nLeaPAJ8.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./lufa-ui-D98RJmy4.js";function s(o){const t={h1:"h1",h2:"h2",h3:"h3",p:"p",...i(),...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(e,{of:h}),`
+`,n.jsx(t.h1,{id:"button",children:"Button"}),`
+`,n.jsx(t.p,{children:"A button is ..."}),`
+`,n.jsx(a,{}),`
+`,n.jsx(t.h2,{id:"props",children:"Props"}),`
+`,n.jsx(c,{}),`
+`,n.jsx(t.h2,{id:"stories",children:"Stories"}),`
+`,n.jsx(t.h3,{id:"color-variants",children:"Color variants"}),`
+`,n.jsx(t.p,{children:"A button can use different colors."}),`
+`,n.jsx(r,{of:m}),`
+`,n.jsx(t.h3,{id:"size-variants",children:"Size variants"}),`
+`,n.jsx(t.p,{children:"A button can use different size."}),`
+`,n.jsx(r,{of:p})]})}function b(o={}){const{wrapper:t}={...i(),...o.components};return t?n.jsx(t,{...o,children:n.jsx(s,{...o})}):s(o)}export{b as default};
