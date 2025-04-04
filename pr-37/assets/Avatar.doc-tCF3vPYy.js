1fcea3fcadfe12001ee1a9f9dd9c97a9800c719d
noofreuuuh <noofreuuuh@users.noreply.github.com>
2025-04-04 00:11:10 +0000

    deploy: ebc2b4825b44d64a5a83bfa4292f5c46ea6ac694

---
+++ pr-37/assets/Avatar.doc-tCF3vPYy.js
@@ -0,0 +1,13 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as l}from"./index-B8rYRX2K.js";import{M as c,P as m,C as t,S as r}from"./index-DDRDyLAb.js";import{A as x,a as i,b as a}from"./Avatar.stories-BHqfyEgK.js";import"./index-D4lIrffr.js";import"./iframe-KK1ToJoN.js";import"./index-DsJinFGm.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./lufa-ui-BrOryYgL.js";function e(s){const n={h1:"h1",h2:"h2",h3:"h3",p:"p",...l(),...s.components};return o.jsxs(o.Fragment,{children:[o.jsx(c,{of:x}),`
+`,o.jsx(n.h1,{id:"avatar",children:"Avatar"}),`
+`,o.jsx(n.p,{children:"An avatar is ..."}),`
+`,o.jsx(m,{}),`
+`,o.jsx(n.h2,{id:"props",children:"Props"}),`
+`,o.jsx(t,{}),`
+`,o.jsx(n.h2,{id:"stories",children:"Stories"}),`
+`,o.jsx(n.h3,{id:"all-sizes",children:"All sizes"}),`
+`,o.jsx(r,{of:i}),`
+`,o.jsx(t,{of:i}),`
+`,o.jsx(n.h3,{id:"all-notification-colors",children:"All notification colors"}),`
+`,o.jsx(r,{of:a}),`
+`,o.jsx(t,{of:a})]})}function v(s={}){const{wrapper:n}={...l(),...s.components};return n?o.jsx(n,{...s,children:o.jsx(e,{...s})}):e(s)}export{v as default};
---
+++ pr-37/assets/Stack.stories-BQ1gfBK7.js
@@ -0,0 +1,38 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{a as r,b as l,c as m,d as s,e as c,f as i,g as u,P as n}from"./lufa-ui-BrOryYgL.js";const A={title:"1. Components/Stack",component:r,parameters:{layout:"centered"},tags:[""]},x={nbPlaceholders:{control:{type:"number"},table:{category:"Customize Story content",defaultValue:{summary:3}}},gap:{control:{type:"inline-radio"},type:{name:"enum",value:Object.values(u)},table:{category:"Properties",defaultValue:{summary:u.normal}}},direction:{control:{type:"inline-radio"},type:{name:"enum",value:Object.values(i)},table:{category:"Properties",defaultValue:{summary:i.vertical}}},padding:{control:{type:"inline-radio"},type:{name:"enum",value:Object.values(c)},table:{category:"Properties",defaultValue:{summary:c.none}}},align:{control:{type:"inline-radio"},type:{name:"enum",value:Object.values(s)},table:{category:"Properties",defaultValue:{summary:s.stretch}}},justify:{control:{type:"inline-radio"},type:{name:"enum",value:Object.values(m)},table:{category:"Properties",defaultValue:{summary:m.start}}},wrap:{control:{type:"inline-radio"},type:{name:"enum",value:Object.values(l)},table:{category:"Properties",defaultValue:{summary:l.nowrap}}}},P={nbPlaceholders:3,gap:u.normal,direction:i.vertical,padding:c.none,align:s.stretch,justify:m.start,wrap:l.nowrap},o={argTypes:x,args:P,render:t=>e.jsx(r,{...t,children:Array.from({length:t.nbPlaceholders}).map((a,p)=>e.jsx(n,{children:p},p))})},b={grow:!0,example:"mode 1"},f={grow:{control:{type:"boolean"},table:{category:"Properties",defaultValue:{summary:"true"}}},example:{control:{type:"inline-radio"},type:{name:"enum",value:["mode 1","mode 2","mode 3","mode 4","mode 5","mode 6"]},table:{category:"Properties",defaultValue:{summary:"false"}}}},d={argTypes:f,args:b,render:({grow:t,example:a})=>e.jsx(e.Fragment,{children:e.jsxs(r,{gap:"normal",direction:"horizontal",align:"center",justify:"space-between",wrap:"nowrap",children:[e.jsx(r.Item,{grow:t&&["mode 1","mode 4","mode 6"].includes(a),children:e.jsx(n,{children:["mode 1","mode 4","mode 6"].includes(a)?"Adjust this item":"Fixed width"})}),e.jsx(r.Item,{grow:t&&["mode 2","mode 4","mode 5"].includes(a),children:e.jsx(n,{children:["mode 2","mode 4","mode 5"].includes(a)?"Adjust this item":"Fixed width"})}),e.jsx(r.Item,{grow:t&&["mode 3","mode 5","mode 6"].includes(a),children:e.jsx(n,{children:["mode 3","mode 5","mode 6"].includes(a)?"Adjust this item":"Fixed width"})})]})})};var g,y,h;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  argTypes,
+  args,
+  render: args => {
+    return <Stack {...args}>
+                {Array.from({
+        length: args.nbPlaceholders
+      }).map((_, index) => <Placeholder key={index}>{index}</Placeholder>)}
+            </Stack>;
+  }
+}`,...(h=(y=o.parameters)==null?void 0:y.docs)==null?void 0:h.source}}};var j,S,w;d.parameters={...d.parameters,docs:{...(j=d.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  argTypes: argTypesStackItem,
+  args: argStackItem,
+  render: ({
+    grow,
+    example
+  }) => {
+    return <>
+                <Stack gap="normal" direction="horizontal" align="center" justify="space-between" wrap="nowrap">
+                    <Stack.Item grow={grow && ['mode 1', 'mode 4', 'mode 6'].includes(example)}>
+                        <Placeholder>
+                            {['mode 1', 'mode 4', 'mode 6'].includes(example) ? 'Adjust this item' : 'Fixed width'}
+                        </Placeholder>
+                    </Stack.Item>
+                    <Stack.Item grow={grow && ['mode 2', 'mode 4', 'mode 5'].includes(example)}>
+                        <Placeholder>
+                            {['mode 2', 'mode 4', 'mode 5'].includes(example) ? 'Adjust this item' : 'Fixed width'}
+                        </Placeholder>
+                    </Stack.Item>
+                    <Stack.Item grow={grow && ['mode 3', 'mode 5', 'mode 6'].includes(example)}>
+                        <Placeholder>
+                            {['mode 3', 'mode 5', 'mode 6'].includes(example) ? 'Adjust this item' : 'Fixed width'}
+                        </Placeholder>
+                    </Stack.Item>
+                </Stack>
+            </>;
+  }
+}`,...(w=(S=d.parameters)==null?void 0:S.docs)==null?void 0:w.source}}};const v=["Default","StackItem"];export{o as Default,d as StackItem,v as __namedExportsOrder,A as default};
