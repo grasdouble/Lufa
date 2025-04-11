7b94b8e4d2d8a466ca0969001fd85432ae9ad3ad
noofreuuuh <dev.n2ofr@gmail.com>
2025-04-11 02:16:33 +0200

    fix

--- packages/microfrontend/main-container/vite.config.js
+++ packages/microfrontend/main-container/vite.config.js
@@ -1,33 +1,42 @@
-import { defineConfig } from "vite";
-import reactPreamblePlugin from "@grasdouble/lufa_plugin_vite_vite-plugin-react-preamble";
-import importMapInjectorPlugin from "@grasdouble/lufa_plugin_vite_vite-plugin-import-map-injector";
+import { defineConfig } from 'vite';
+import reactPreamblePlugin from '@grasdouble/lufa_plugin_vite_vite-plugin-react-preamble';
+import importMapInjectorPlugin from '@grasdouble/lufa_plugin_vite_vite-plugin-import-map-injector';
+import { externalizeDeps } from 'vite-plugin-externalize-deps';
 
 export default defineConfig({
-  plugins: [
-    importMapInjectorPlugin({
-      extImportMap: "src/importMapExternal.json",
-      devImportMap: "src/importMap.dev.json",
-      prodImportMap: "src/importMap.json",
-    }),
-    reactPreamblePlugin(),
-  ],
-  build: {
-    target: "esnext",
-    modulePreload: false, // Single-SPA manages the loading of modules
-    minify: false,
-    rollupOptions: {
-      output: {
-        format: "esm",
-        entryFileNames: "[name].js",
-      },
+    plugins: [
+        importMapInjectorPlugin({
+            extImportMap: 'src/importMapExternal.json',
+            devImportMap: 'src/importMap.dev.json',
+            prodImportMap: 'src/importMap.json',
+        }),
+        externalizeDeps({
+            deps: false,
+            devDeps: false,
+            optionalDeps: false,
+            peerDeps: true,
+            except: [],
+            nodeBuiltins: true,
+        }),
+        reactPreamblePlugin(),
+    ],
+    build: {
+        target: 'esnext',
+        modulePreload: false, // Single-SPA manages the loading of modules
+        minify: false,
+        rollupOptions: {
+            output: {
+                format: 'esm',
+                entryFileNames: '[name].js',
+            },
+        },
     },
-  },
-  server: {
-    port: 5173,
-    cors: true,
-    hmr: true,
-  },
-  preview: {
-    port: 5173,
-  },
+    server: {
+        port: 5173,
+        cors: true,
+        hmr: true,
+    },
+    preview: {
+        port: 5173,
+    },
 });
