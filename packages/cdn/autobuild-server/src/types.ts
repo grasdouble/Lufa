b748db81861df1b6823f43421e036ca3981b18ee
noofreuuuh <dev.n2ofr@gmail.com>
2025-04-17 01:18:21 +0200

    fix(cdn): Make rate limit based on IP to no block all users

---
+++ packages/cdn/autobuild-server/src/types.ts
@@ -0,0 +1,23 @@
+export interface PackageJson {
+    name: string;
+    version: string;
+    type?: 'module' | 'commonjs';
+    main?: string;
+    module?: string;
+    exports?: Record<string, string | { import?: string; default?: string }>;
+    peerDependencies?: Record<string, string>;
+}
+
+export interface ExtractedParams {
+    scope?: string;
+    exportPath?: string;
+    fullName: string;
+    dirName: string;
+    cdnPkgPath: string;
+    tmpPkgPath: string;
+}
+
+export interface LoadLibraryResult {
+    status: number;
+    message: string;
+}
