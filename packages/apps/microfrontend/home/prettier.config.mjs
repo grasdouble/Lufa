8f2eb353f2b976178ccdf16598aa71fcec51582e
noofreuuuh <dev.n2ofr@gmail.com>
2025-12-13 12:04:04 +0100

    fix

--- packages/apps/microfrontend/home/prettier.config.mjs
+++ packages/apps/microfrontend/home/prettier.config.mjs
@@ -5,6 +5,7 @@ import sharedConfig from '@grasdouble/lufa_config_prettier/prettier.config.mjs';
  */
 const config = {
     ...sharedConfig,
+    plugins: [...(sharedConfig.plugins || []), 'prettier-plugin-tailwindcss'],
 };
 
 export default config;
