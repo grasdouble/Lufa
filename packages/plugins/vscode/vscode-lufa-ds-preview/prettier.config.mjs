cf70df8250d99446c1ce72070127ca12e8e7aefa
noofreuuuh <dev.n2ofr@gmail.com>
2026-01-06 18:50:20 +0100

    fix(prettier): wrong prettier config on vscode-lufa-ds-preview

--- packages/plugins/vscode/vscode-lufa-ds-preview/prettier.config.mjs
+++ packages/plugins/vscode/vscode-lufa-ds-preview/prettier.config.mjs
@@ -1,1 +1,10 @@
-export { default } from '@grasdouble/lufa_config_prettier';
+import sharedConfig from '@grasdouble/lufa_config_prettier/prettier.config.mjs';
+
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+  ...sharedConfig,
+};
+
+export default config;
