e00b0a33b85459693863a328406ee66e7ba0022f
noofreuuuh <dev.n2ofr@gmail.com>
2026-02-07 03:19:43 +0100

    improve ci

--- packages/design-system/playwright/playwright-ct.config.ts
+++ packages/design-system/playwright/playwright-ct.config.ts
@@ -18,7 +18,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'line',
+  reporter: process.env.CI
+    ? [
+        ['line'],
+        ['html', { outputFolder: 'playwright-report', open: 'never' }],
+        ['json', { outputFile: 'test-results/results.json' }],
+        ['junit', { outputFile: 'test-results/junit.xml' }],
+      ]
+    : [['line'], ['html', { outputFolder: 'playwright-report', open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
